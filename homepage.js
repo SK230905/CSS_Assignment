4c47ec07006bf3eac155f30c95fccae77384e37c
Shavelin Khoo <s10259194@connect.np.edu.sg>
2025-02-08 21:23:15 +0800

    Updated 2123

--- homepage.js
+++
@@ -1,7 +0,0 @@
-let currentAngle = 0;
-
-function rotateCarousel(degree) {
-    const carousel = document.getElementById('carousel');
-    currentAngle += degree;
-    carousel.style.transform = `rotateY(${currentAngle}deg)`;
-}
